62ad68cb71ad3093d39722c62c0f8a726828fd04
sahil04sharma <interactsahil5232@gmail.com>
2026-02-23 12:56:10 +0530

    url update

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://savra-e3iu.onrender.com/api/auth/login",
         { email, password }
       );
 

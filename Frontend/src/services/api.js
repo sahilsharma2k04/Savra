0ec06b5e8fd22513fc0c9adbd5f8b651cd0a0ca1
sahil04sharma <interactsahil5232@gmail.com>
2026-02-23 12:45:53 +0530

    url

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/dashboard",
+  baseURL: "https://savra-e3iu.onrender.com/api/dashboard",
 });
 
 API.interceptors.request.use((config) => {
